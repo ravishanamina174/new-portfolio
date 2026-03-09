043b108ff2d29a415fa423df862b8b47e74dbaa0
ravisha <ravishanamina174@gmail.com>
2026-03-09 16:51:59 +0530

    new 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
                 yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋🏻`}
               />
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
@@ -126,15 +126,6 @@ export default function Page() {
           ))}
         </div>
       </section>
-
-
-
-
-
-
-
-
-
 
 
       <section id="skills">
